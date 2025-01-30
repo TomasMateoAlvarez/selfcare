789a3e9a5ff55cdd3588d0a5b9ad97541c668ed0
Mateo <mateoac439@gmail.com>
2025-01-30 16:04:30 -0300

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Deshabilita la regla de variables no utilizadas
+      "react/no-unescaped-entities": "off", // Deshabilita la regla de entidades no escapadas
+    },
+  },
 ];
 
 export default eslintConfig;
